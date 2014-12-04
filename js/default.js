70c7c1966b07bafe5ff133c6f691140ea7c12be0
barceloco <github@armandniederberger.com>
2014-12-03 18:47:34 -0800

    holoide commit

--- js/default.js
+++ js/default.js
@@ -10,41 +10,6 @@ window.onload = function (){
 
 function Init(){
   scene = new THREE.Scene();
-  var helloWorldGeometry = new THREE.TextGeometry(
-    "Hello World",
-    {
-      size: 12,
-      height: 2,
-      curveSegments: 4,
-      font: "helvetiker",
-      weight: "normal",
-      style: "normal",
-      bevelThickness: 0.5,
-      bevelSize: 0.25,
-      bevelEnabled: true,
-      material: 0,
-      extrudeMaterial: 1
-    }
-  );
-  var material = new THREE.MeshFaceMaterial(
-    [ 
-      new THREE.MeshPhongMaterial(
-        {
-          color: 0xffffff,
-          shading: THREE.FlatShading
-        }
-      ), // front
-      new THREE.MeshPhongMaterial(
-        {
-          color: 0xffffff,
-          shading: THREE.SmoothShading
-        }
-      ) // side
-    ]
-  );
-  var helloWorldMesh = new THREE.Mesh( helloWorldGeometry, material );
-  scene.add(helloWorldMesh);
-  
 
   //setup camera
   camera = new LeiaCamera();
@@ -72,46 +37,78 @@ function Init(){
   addLights();
  }
 
- function animate() 
- {
- 	requestAnimationFrame( animate );
-    renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0)); 
-	renderer.Leia_render(scene, camera,undefined,undefined,_holoScreenSize,_camFov,_messageFlag);
- }
-  
+function animate() 
+{
+  requestAnimationFrame( animate );
+  renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0)); 
+  renderer.Leia_render(scene, camera, undefined, undefined, _holoScreenSize, _camFov, _messageFlag);
+}
   
 
 function addObjectsToScene(){
-    //Add your objects here
-  LEIA_setBackgroundPlane('resource/brickwall_900x600_small.jpg');
+  //Add your objects here
+
+  // background Plane
+  var planeTexture = new THREE.ImageUtils.loadTexture( 'resource/brickwall_900x600_small.jpg' );
+  planeTexture.wrapS = planeTexture.wrapT = THREE.RepeatWrapping; 
+  planeTexture.repeat.set( 1, 1 );
+  var planeMaterial = new THREE.MeshPhongMaterial( {map: planeTexture, color: 0xffdd99 } );
+  var planeGeometry = new THREE.PlaneGeometry(80, 60, 10, 10);
+  plane = new THREE.Mesh(planeGeometry,   planeMaterial);
+  plane.position.z = -6;
+  plane.castShadow = false;
+  plane.receiveShadow = true;
+  scene.add(plane);
+
+  // hello world text
+  var helloWorldGeometry = new THREE.TextGeometry(
+    "Hello World",
+    {
+      size: 12,
+      height: 2,
+      curveSegments: 4,
+      font: "helvetiker",
+      weight: "normal",
+      style: "normal",
+      bevelThickness: 0.5,
+      bevelSize: 0.25,
+      bevelEnabled: true,
+      material: 0,
+      extrudeMaterial: 1
+    }
+  );
+  var helloWorldMaterial = new THREE.MeshFaceMaterial(
+    [ 
+      new THREE.MeshPhongMaterial(
+        {
+          color: 0xffffff,
+          shading: THREE.FlatShading
+        }
+      ), // front
+      new THREE.MeshPhongMaterial(
+        {
+          color: 0xffffff,
+          shading: THREE.SmoothShading
+        }
+      ) // side
+    ]
+  );
+  var helloWorldMesh = new THREE.Mesh( helloWorldGeometry, helloWorldMaterial );
+  helloWorldMesh.position.z = 10;
+  scene.add(helloWorldMesh);  
 }
 
 function addLights(){
     //Add Lights Here
- 	var light = new THREE.SpotLight( 0xffffff);
-	//light.color.setHSL( Math.random(), 1, 0.5 );
- 	light.position.set(0,60,60);
-    light.shadowCameraVisible = false;
-    light.castShadow = true;
-    light.shadowMapWidth = light.shadowMapHeight = 512;
-    light.shadowDarkness = 0.7;
- 	scene.add(light);
+ 	var spotLight = new THREE.SpotLight( 0xffffff);
+ 	spotLight.position.set(0,60,60);
+    spotLight.shadowCameraVisible = false;
+    spotLight.castShadow = true;
+    spotLight.shadowMapWidth = spotLight.shadowMapHeight = 512;
+    spotLight.shadowDarkness = 0.7;
+ 	scene.add(spotLight);
   
  	var ambientLight = new THREE.AmbientLight(0x222222);	
  	scene.add(ambientLight);
 }
 
-function LEIA_setBackgroundPlane(filename, aspect){
-	var planeTexture = new THREE.ImageUtils.loadTexture( filename );
-	planeTexture.wrapS = planeTexture.wrapT = THREE.RepeatWrapping; 
-	planeTexture.repeat.set( 1, 1 );
-	
-  //
-    var planeMaterial = new THREE.MeshPhongMaterial( {map: planeTexture, color: 0xffdd99 } );
-    var planeGeometry = new THREE.PlaneGeometry(80, 60, 10, 10);
-	plane = new THREE.Mesh(planeGeometry,   planeMaterial);
-	plane.position.z = -6;
-	plane.castShadow = false;
-	plane.receiveShadow = true;
-	scene.add(plane);
-}
