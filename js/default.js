ec642465106817a5bc882fd7af5c63135b69a67d
liyanguestc <liyanguestc@gmail.com>
2014-12-05 18:06:30 +0800

    holoide commit

--- js/default.js
+++ js/default.js
@@ -1,125 +1,122 @@
-var windowWidth = window.innerWidth, windowHeight = window.innerHeight;
-var camera,renderer,scene;
+var windowWidth = window.innerWidth,
+    windowHeight = window.innerHeight;
+var camera, renderer, scene;
 
 // add your global variables here:
 var helloWorldMesh;
 
-window.onload = function (){
-     console.log("onload");
-     Init();
-     animate();
- };
+window.onload = function() {
+    console.log("onload");
+    Init();
+    animate();
+};
 
-function Init(){
-  scene = new THREE.Scene();
+function Init() {
+    scene = new THREE.Scene();
 
-  // camera setup
-  camera = new LeiaCamera({
-         cameraPosition:new THREE.Vector3(_camPosition.x, _camPosition.y, _camPosition.z), 
-		 targetPosition: new THREE.Vector3(_tarPosition.x, _tarPosition.y, _tarPosition.z)
-        });
-  scene.add(camera);
-  
-  // rendering setup
-  renderer = new LeiaWebGLRenderer({
-    antialias:true, 
-    renderMode: _renderMode, 
-    shaderMode: _nShaderMode,
-    colorMode: _colorMode,
-    devicePixelRatio: 1 
-  });
-  renderer.Leia_setSize( windowWidth, 0.75*windowWidth );
-  renderer.shadowMapEnabled = true;
-  renderer.shadowMapSoft = true;
-  document.body.appendChild( renderer.domElement );
-  
-  //add object to scene
-  addObjectsToScene();
-  
-  //add Light
-  addLights();
- }
+    // camera setup
+    camera = new LeiaCamera({
+        cameraPosition: new THREE.Vector3(_camPosition.x, _camPosition.y, _camPosition.z),
+        targetPosition: new THREE.Vector3(_tarPosition.x, _tarPosition.y, _tarPosition.z)
+    });
+    scene.add(camera);
 
-function animate() 
-{
-  requestAnimationFrame( animate );
-  renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0)); 
-  renderer.Leia_render({
-     scene:scene, 
-     camera:camera,
-     holoScreenSize:_holoScreenSize,
-     holoCamFov:_camFov,
-     messageFlag:_messageFlag
-   });
-}
-  
-function addObjectsToScene(){     // Add your objects here
-  // background Plane
-  var planeTexture = new THREE.ImageUtils.loadTexture( 'resource/brickwall_900x600_small.jpg' );
-  planeTexture.wrapS = planeTexture.wrapT = THREE.RepeatWrapping; 
-  planeTexture.repeat.set( 1, 1 );
-  var planeMaterial = new THREE.MeshPhongMaterial( {map: planeTexture, color: 0xffdd99 } );
-  var planeGeometry = new THREE.PlaneGeometry(80, 60, 10, 10);
-  plane = new THREE.Mesh(planeGeometry,   planeMaterial);
-  plane.position.z = -6;
-  plane.castShadow = false;
-  plane.receiveShadow = true;
-  scene.add(plane);
+    // rendering setup
+    renderer = new LeiaWebGLRenderer({
+        antialias: true,
+        renderMode: _renderMode,
+        shaderMode: _nShaderMode,
+        colorMode: _colorMode,
+        devicePixelRatio: 1
+    });
+    renderer.Leia_setSize(windowWidth, 0.75 * windowWidth);
+    renderer.shadowMapEnabled = true;
+    renderer.shadowMapSoft = true;
+    document.body.appendChild(renderer.domElement);
 
-  // hello world text
-  var helloWorldGeometry = new THREE.TextGeometry(
-    "Hello World",
-    {
-      size: 9,
-      height: 2,
-      curveSegments: 4,
-      font: "helvetiker",
-      weight: "normal",
-      style: "normal",
-      bevelThickness: 0.5,
-      bevelSize: 0.25,
-      bevelEnabled: true,
-      material: 0,
-      extrudeMaterial: 1
-    }
-  );
-  helloWorldGeometry.computeBoundingBox();
-  var hwbb = helloWorldGeometry.boundingBox;
-  var hwbbx = -0.5*(hwbb.max.x - hwbb.min.x);
-  var hwbby = -0.5*(hwbb.max.y - hwbb.min.y);
-  var helloWorldMaterial = new THREE.MeshFaceMaterial(
-    [ 
-      new THREE.MeshPhongMaterial(
-        {
-          color: 0xffffff,
-          shading: THREE.FlatShading
-        }
-      ), // front
-      new THREE.MeshPhongMaterial(
-        {
-          color: 0xffffff,
-          shading: THREE.SmoothShading
-        }
-      ) // side
-    ]
-  );
-  var helloWorldMesh = new THREE.Mesh( helloWorldGeometry, helloWorldMaterial );
-  helloWorldMesh.castShadow = true;
-  helloWorldMesh.position.set(hwbbx, hwbby, 0);
-  scene.add(helloWorldMesh);  
+    //add object to scene
+    addObjectsToScene();
+
+    //add Light
+    addLights();
 }
 
-function addLights(){    
-   //Add Lights Here
- 	var spotLight = new THREE.SpotLight( 0xffffff);
- 	spotLight.position.set(0,0,70);
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0));
+    renderer.Leia_render({
+        scene: scene,
+        camera: camera,
+        holoScreenSize: _holoScreenSize,
+        holoCamFov: _camFov,
+        messageFlag: _messageFlag
+    });
+}
+
+function addObjectsToScene() { // Add your objects here
+    // background Plane
+    var planeTexture = new THREE.ImageUtils.loadTexture('resource/brickwall_900x600_small.jpg');
+    planeTexture.wrapS = planeTexture.wrapT = THREE.RepeatWrapping;
+    planeTexture.repeat.set(1, 1);
+    var planeMaterial = new THREE.MeshPhongMaterial({
+        map: planeTexture,
+        color: 0xffdd99
+    });
+    var planeGeometry = new THREE.PlaneGeometry(80, 60, 10, 10);
+    plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.position.z = -6;
+    plane.castShadow = false;
+    plane.receiveShadow = true;
+    scene.add(plane);
+
+    // hello world text
+    var helloWorldGeometry = new THREE.TextGeometry(
+        "Hello World", {
+            size: 9,
+            height: 2,
+            curveSegments: 4,
+            font: "helvetiker",
+            weight: "normal",
+            style: "normal",
+            bevelThickness: 0.5,
+            bevelSize: 0.25,
+            bevelEnabled: true,
+            material: 0,
+            extrudeMaterial: 1
+        }
+    );
+    helloWorldGeometry.computeBoundingBox();
+    var hwbb = helloWorldGeometry.boundingBox;
+    var hwbbx = -0.5 * (hwbb.max.x - hwbb.min.x);
+    var hwbby = -0.5 * (hwbb.max.y - hwbb.min.y);
+    var helloWorldMaterial = new THREE.MeshFaceMaterial(
+        [
+            new THREE.MeshPhongMaterial({
+                color: 0xffffff,
+                shading: THREE.FlatShading
+            }), // front
+            new THREE.MeshPhongMaterial({
+                color: 0xffffff,
+                shading: THREE.SmoothShading
+            }) // side
+        ]
+    );
+    var helloWorldMesh = new THREE.Mesh(helloWorldGeometry, helloWorldMaterial);
+    helloWorldMesh.castShadow = true;
+    helloWorldMesh.position.set(hwbbx, hwbby, 0);
+    scene.add(helloWorldMesh);
+}
+
+function addLights() {
+    //Add Lights Here
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(0, 0, 70);
     spotLight.shadowCameraVisible = false;
     spotLight.castShadow = true;
     spotLight.shadowMapWidth = spotLight.shadowMapHeight = 512;
     spotLight.shadowDarkness = 0.7;
- 	scene.add(spotLight);
-  
- 	var ambientLight = new THREE.AmbientLight(0x222222);	
- 	scene.add(ambientLight);
-}
+    scene.add(spotLight);
 
+    var ambientLight = new THREE.AmbientLight(0x222222);
+    scene.add(ambientLight);
+}
