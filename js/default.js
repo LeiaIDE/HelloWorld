dde85ca8dbff938cfe6d9a52dfafd34b2b3bbdb6
barceloco <github@armandniederberger.com>
2014-12-05 18:49:46 -0800

    holoide commit

--- js/default.js
+++ js/default.js
@@ -15,7 +15,7 @@ function Init() {
     scene = new THREE.Scene();
 
     // camera setup
-    camera = new LeiaCamera({
+    camera = new LeiaCamera({ 
         cameraPosition: new THREE.Vector3(_camPosition.x, _camPosition.y, _camPosition.z),
         targetPosition: new THREE.Vector3(_tarPosition.x, _tarPosition.y, _tarPosition.z)
     });
